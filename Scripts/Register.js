3ddd2028e8c9495074db1ea8d933d6f9ea487e4b
OyasumiOfficial <theoyasumiprofissional@gmail.com>
2026-07-27 16:05:50 -0300

    Downloaded Node and Express

--- Scripts/Register.js
+++ Scripts/Register.js
@@ -1,3 +1,5 @@
+import express from `express`
+
 const Email = document.querySelector('#Email');
 const Username = document.querySelector('#Username');
 const Password = document.querySelector('#Password');
